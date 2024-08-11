16e9a3fc1ef89d30b022dfe483a7fdde4f89037c
Aniket Vishwakarma <sharmaaniket396@gmail.com>
2024-08-11 23:21:03 +0530

    Added Functions

--- Script.js
+++ Script.js
@@ -1,1 +1,55 @@
-console.log("Hello World! I DOun't give a bug.")
+console.log("Hello World! I don't give a bug.")
+function myFunction(){
+    console.log("Hello");
+    console.log("How are you?");
+}
+myFunction()
+
+let name="anni";
+ function myFunction2(){
+    let num=22;
+    console.log(name);
+    console.log(num);
+ }
+ myFunction2();
+
+ let x,y,sum;
+  x=23;
+  y=56;
+  sum=x+y;
+  console.log(sum);
+
+  let x1,num,sum1;
+  x1=35;
+  y1=45;
+  num=x1/2;
+  sum1=x1+y1;
+  console.log(num+"\n"+sum1); //here "\n" is used to give line break to a string.
+
+// it stores string data type
+let txt="Hey developers.";
+
+// It stores integer data type.
+let a=4;
+let b=67;
+
+// it stores boolean data type.
+console.log(a==b);
+
+// to check Strictly (i.e. Whether the datatypes of both 
+// variables are same.) === is used 
+console.log(a===b); // ---> returns true to console.
+
+// it stores array datatype
+let places=["Haridwar","Somnath","kedarnath"];
+console.log(places);
+
+// it  stores object data(objects are represented in the below way mainly)
+
+let Student={
+    firstName:"Anni",
+    lastName:"Vishwakarma",
+    age:22,
+    PhoneNumber:9369662871
+}
+console.log(Student);
